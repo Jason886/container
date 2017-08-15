19e70b8117bede4e9fc437df61f9401ee7bb8270
jason886 <359152155@qq.com>
2017-08-15 17:44:31 +0800

    add header

--- src/con.h
+++ src/con.h
@@ -1,9 +1,15 @@
-/*
- * author: jason
- * mail: 359152155@qq.com
- * github: https://github.com/Jason886
- *
- */
+/*******************************************************************************
+* Copyright (C), 2017-2022, 359152155@qq.com.
+*
+* FileName    : con.h
+* Author      : xianchen.p
+* Date        : 2017-08-15 
+* Description : export all headers
+* Github      : https://github.com/Jason886/container
+*------------------------------------------------------------------------------*
+* Record      :
+*******************************************************************************/
+
 #ifndef _CON_H_
 #define _CON_H_
 
